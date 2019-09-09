4289d2af9a2667bde56996019eb9ede890605989
v3nd3tt4 <pilopaokta@gmail.com>
2019-09-09 22:11:48 +0700

    api login n register

---
+++ application/controllers/Api.php
@@ -0,0 +1,126 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function login()
+	{
+		$username = $this->input->post('username', true);
+    	$password = $this->input->post('password', true);
+
+    	$get = $this->db->query("select * from tb_user where email = '$username' ");
+
+    	if($get->num_rows() == 0){
+	        $return = array('status' => 'gagal', 'message' => 'username tidak ditemukan');
+			echo json_encode($return);
+    	}else{
+    		$hash = $get->row()->password;
+    		if (password_verify($password, $hash)) {
+    			$sess = array(
+    				'login' => true,
+    				'id_user' => $get->row()->id_user,
+    				'level' => $get->row()->level,
+                    'nama_user' => $get->row()->nama
+    			);
+    			$this->session->set_userdata($sess);
+    			$return = array('status' => 'sukses', 'message' => 'login berhasil');
+				echo json_encode($return);
+			} else {
+			    $return = array('status' => 'gagal', 'message' => 'password salah');
+				echo json_encode($return);
+			}
+    	}
+	}
+
+	public function tambah(){
+		$data = array(
+			'page' => 'user/tambah',
+			'link' => 'user',
+			'script' => 'user/script'
+		);
+		$this->load->view('template_srtdash/wrapper', $data);
+	}
+
+	public function register(){
+		$data = array(
+			'nama'	=> $this->input->post('nama', true),
+			'email'	=> $this->input->post('email', true),
+			'no_hp'	=> $this->input->post('no_hp', true),
+			'alamat'	=> $this->input->post('alamat', true),
+			'password'	=> password_hash($this->input->post('password', true), PASSWORD_DEFAULT),
+			'level'=> 'guest',
+			'jenis_kelamin'=> $this->input->post('jk', true),
+		);
+		$save = $this->db->insert('tb_user', $data);
+		if($save){
+			$return = array('status' => 'sukses', 'message' => 'sukses mendaftar');
+			echo json_encode($return);
+		}else{
+			$return = array('status' => 'gagal', 'message' => 'gagal mendaftar');
+			echo json_encode($return);
+		}
+	}
+
+	public function hapus($id){
+		$hapus = $this->db->delete('tb_user', array('id_user' => $id));
+		if($hapus){
+			echo '<script>alert("Berhasil dihapus!!");window.history.back();</script>';
+		}else{
+			echo '<script>alert("Gagal dihapus!!");window.history.back();</script>';
+		}
+	}
+
+	public function edit($id){
+		$data = array(
+			'page' => 'user/edit',
+			'link' => 'user',
+			'script' => 'user/script',
+			'data_user' => $this->db->query("select * from tb_user where id_user = '$id'")
+		);
+		$this->load->view('template_srtdash/wrapper', $data);
+	}
+
+	public function update(){
+		if($this->input->post('password', true) == ''){
+			$data = array(
+				'nama'	=> $this->input->post('nama', true),
+				'email'	=> $this->input->post('email', true),
+				'no_hp'	=> $this->input->post('no_hp', true),
+				'alamat'	=> $this->input->post('alamat', true),
+				'level'=> $this->input->post('level', true),
+				'jenis_kelamin'=> $this->input->post('jk', true),
+			);
+		}else{
+			$data = array(
+				'nama'	=> $this->input->post('nama', true),
+				'email'	=> $this->input->post('email', true),
+				'no_hp'	=> $this->input->post('no_hp', true),
+				'alamat'	=> $this->input->post('alamat', true),
+				'password'	=> password_hash($this->input->post('password', true), PASSWORD_DEFAULT),
+				'level'=> $this->input->post('level', true),
+				'jenis_kelamin'=> $this->input->post('jk', true),
+			);
+		}
+		$save = $this->db->update('tb_user', $data, array('id_user' => $this->input->post('id_user', true)));
+		if($save){
+			echo '<script>alert("Berhasil diupdate!!");window.location.href = "'.base_url().'user";</script>';
+		}else{
+			echo '<script>alert("Gagal diupdate!!");window.history.back();</script>';
+		}
+	}
+}
