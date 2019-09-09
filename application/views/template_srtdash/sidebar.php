a9141034d8879dfad380fbce7589cedc373a45d2
v3nd3tt4 <pilopaokta@gmail.com>
2019-09-09 20:25:50 +0700

    first commit

---
+++ application/views/template_srtdash/sidebar.php
@@ -0,0 +1,29 @@
+<!-- sidebar menu area start -->
+    <div class="sidebar-menu">
+        <div class="sidebar-header">
+            <div class="logo">
+                <a href="index.html">
+                    <h1 style="color: #fff">WO</h1>
+                    <!-- <img src="<?=base_url()?>assets_srtdash/images/icon/logo.png" alt="logo"> -->
+                </a>
+            </div>
+        </div>
+        <div class="main-menu">
+            <div class="menu-inner">
+                <nav>
+                    <ul class="metismenu" id="menu">
+                        <li class="active"><a href="<?=base_url()?>"><i class="ti-map-alt"></i> <span>dashboard</span></a></li>
+                        <li class="active">
+                            <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Master Data</span></a>
+                            <ul class="collapse">
+                                <li class="active"><a href="index.html">User</a></li>
+                                <li><a href="index2.html">Gedung</a></li>
+                                <li><a href="index3.html">Paket</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+<!-- sidebar menu area end -->
