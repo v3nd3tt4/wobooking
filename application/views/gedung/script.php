a1bdecd95cf13a536eb84b8a5a8167fd0b03115c
Arif Setiawan <arif.setiawan@staff.itera.ac.id>
2019-09-10 23:41:33 +0700

    paket new

--- application/views/gedung/script.php
+++ application/views/gedung/script.php
@@ -1,5 +1,29 @@
 <script type="text/javascript">
 	$(document).ready( function () {
 	    $('#myTable').DataTable();
+
+	    $(document).on('click', '.btn-tambah-ket', function(e){
+	    	e.preventDefault();
+	    	var row = '';
+	    	var ket = $('.keterangan').val();
+	    	var harga_ket = $('.harga_keterangan').val();
+	    	row += '<tr>';
+	    		row += '<td>';
+	    			row += '<input type="text" name="keterangan_r[]" value="'+ket+'" class="form-control"/>';
+	    		row += '</td>';
+	    		row += '<td>';
+	    			row += '<input type="text" name="harga_keterangan_r[]" value="'+harga_ket+'" class="form-control"/>';
+	    		row += '</td>';
+	    		row += '<td>';
+	    			row += '<button class="btn btn-sm btn-danger hapus_ket">Hapus</button>';
+	    		row += '</td>';
+	    	row += '</tr>';
+	    	$('.table-ket tbody').append(row);
+	    });
+
+	    $(document).on('click', '.hapus_ket', function(e){
+	    	e.preventDefault();
+	    	$(this).parent().parent().remove();
+	    });
 	} );
 </script>
--- application/views/gedung/tambah_paket.php
+++ application/views/gedung/tambah_paket.php
@@ -6,7 +6,7 @@
     <!-- sales report area end -->
     <!-- overview area start -->
     <div class="row">
-        <div class="col-xl-12">
+        <div class="col-xl-8">
             <div class="card">
                 <div class="card-body">
                     <div class="d-flex justify-content-between align-items-center">
@@ -21,17 +21,44 @@
                             <input type="hidden" name="id_gedung" value="<?=$id_gedung?>">
                             <input type="text" name="nama_paket" class="form-control" required />
                         </div>
-                        <div class="form-group">
-                            <label>Harga Paket:</label>
-                            <input type="number" name="harga_paket" class="form-control" required />
-                        </div>
+                        <table id="table_ket" class="table table-striped table-ket">
+                            <thead>
+                                <tr>
+                                    <th>Keterangan</th>
+                                    <th>Harga</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
                         
                         <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Simpan</button>
                     </form>
                 </div>
             </div>
         </div>
-        
+        <div class="col-xl-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h4 class="header-title mb-0">Keterangan</h4>
+                    </div>
+
+                    <hr/>
+                        <div class="form-group">
+                            <label>Nama Paket:</label>
+                            <input type="text" name="keterangan" class="keterangan form-control" required />
+                        </div>
+                        <div class="form-group">
+                            <label>Harga Paket:</label>
+                            <input type="number" name="harga_keterangan" class="harga_keterangan form-control" required />
+                        </div>
+                        
+                        <button type="button" class="btn btn-danger btn-tambah-ket"><i class="fa fa-plus"></i> Tambah</button>
+                </div>
+            </div>
+        </div>
     </div>
     <!-- overview area end -->
 </div>
