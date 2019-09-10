1910652a54c05ba254e82ed0523ec3930aa87960
Arif Setiawan <arif.setiawan@staff.itera.ac.id>
2019-09-10 21:39:36 +0700

    a

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wobooking/';
+
+if (basename(FCPATH)!='wobooking') 
+	$dir = '';
+else
+	$dir = basename(FCPATH);
+
+$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/wobooking';
+
+
+//$config['base_url'] = 'http://localhost/wobooking';
 
 /*
 |--------------------------------------------------------------------------
