66a9375dfa74a02f0ae003a485c7ecaf7b1a4902
Arif Setiawan <arif.setiawan@staff.itera.ac.id>
2019-09-13 22:04:22 +0700

    a

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -320,13 +320,13 @@ class Api extends CI_Controller {
 		$query = $this->db->query("SELECT * from tb_pesan_gedung where id_user = '$id_user'");
 		$result = array();
 		foreach ($query->result() as $value) {
-			$query2 = $this->db->query("SELECT tp_paket.id_paket, tp_paket.nama_paket, tb_pesan_gedung.jam_sewa_awal,
+			$query2 = $this->db->query("SELECT tb_paket.id_paket, tb_paket.nama_paket, tb_pesan_gedung.jam_sewa_awal,
 			tb_pesan_gedung.jam_sewa_akhir, tb_pesan_gedung.tanggal_sewa, tb_pesan_gedung.status, 
 			tb_pesan_gedung.nama_pemesan, tb_pesan_gedung.keterangan, tb_gedung.nama_gedung from tb_paket 
 			left join tb_pesan_gedung on tb_pesan_gedung.id_paket=tb_paket.id_paket
 			left join tb_gedung on tb_gedung.id_gedung = tb_paket.id_gedung
 			 where tb_paket.id_paket = '".$value->id_paket."'");
-			 
+
 			$result[] = $value;
 		}
 		if($query->num_rows() != 0){
