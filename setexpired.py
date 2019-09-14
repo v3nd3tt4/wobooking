6e91d36b18b9dd6d91ce4c5cb1cfdd8475f170ff
v3nd3tt4 <pilopaokta@gmail.com>
2019-09-14 20:48:23 +0700

    python to expired

---
+++ setexpired.py
@@ -0,0 +1,21 @@
+from orator import DatabaseManager
+
+config = {
+    'mysql': {
+        'driver': 'mysql',
+        'host': 'localhost',
+        'database': 'db_wo',
+        'user': 'root',
+        'password': 'goldroger27',
+        'prefix': ''
+    }
+}
+
+db = DatabaseManager(config)
+results = db.select('SELECT * from tb_pesan_gedung where (DATE_ADD(NOW(), INTERVAL 2 HOUR) > waktu_pesan) and status = "pending"')
+
+
+# print results
+for r in results:
+	db.table('tb_pesan_gedung').where('id_pesan', 2).update(status='expired')
+	
