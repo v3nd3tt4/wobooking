2b854567ba2191d65d289e82d890273e60790b90
v3nd3tt4 <pilopaokta@gmail.com>
2019-09-14 15:41:38 +0700

    up

--- application/views/dashboard_srtdash.php
+++ application/views/dashboard_srtdash.php
@@ -1,37 +1,37 @@
 <div class="main-content-inner">
     <!-- sales report area start -->
     <div class="sales-report-area mt-5 mb-5">
-        <!-- <div class="row">
+        <div class="row">
             <div class="col-md-4">
                 <div class="single-report mb-xs-30">
                     <div class="s-report-inner pr--20 pt--30 mb-3">
-                        <div class="icon"><i class="fa fa-btc"></i></div>
+                        <div class="icon"><i class="fa fa-building"></i></div>
                         <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Bitcoin</h4>
-                            <p>24 H</p>
+                            <h4 class="header-title mb-0">Gedung</h4>
+                            <!-- <p>24 H</p> -->
                         </div>
                         <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
+                            <h2><?=$data_member->row()->jml_member?></h2>
+                            <!-- <span>- 45.87</span> -->
                         </div>
                     </div>
-                    <canvas id="coin_sales1" height="100"></canvas>
+                    <!-- <canvas id="coin_sales1" height="100"></canvas> -->
                 </div>
             </div>
             <div class="col-md-4">
                 <div class="single-report mb-xs-30">
                     <div class="s-report-inner pr--20 pt--30 mb-3">
-                        <div class="icon"><i class="fa fa-btc"></i></div>
+                        <div class="icon"><i class="fa fa-user"></i></div>
                         <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Bitcoin Dash</h4>
-                            <p>24 H</p>
+                            <h4 class="header-title mb-0">Member</h4>
+                            <!-- <p>24 H</p> -->
                         </div>
                         <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
+                            <h2><?=$data_gedung->row()->jml_gedung?></h2>
+                            <!-- <span>- 45.87</span> -->
                         </div>
                     </div>
-                    <canvas id="coin_sales2" height="100"></canvas>
+                    <!-- <canvas id="coin_sales2" height="100"></canvas> -->
                 </div>
             </div>
             <div class="col-md-4">
@@ -39,18 +39,18 @@
                     <div class="s-report-inner pr--20 pt--30 mb-3">
                         <div class="icon"><i class="fa fa-eur"></i></div>
                         <div class="s-report-title d-flex justify-content-between">
-                            <h4 class="header-title mb-0">Euthorium</h4>
-                            <p>24 H</p>
+                            <h4 class="header-title mb-0">Pemesanan</h4>
+                            <!-- <p>24 H</p> -->
                         </div>
                         <div class="d-flex justify-content-between pb-2">
-                            <h2>$ 4567809,987</h2>
-                            <span>- 45.87</span>
+                            <h2><?=$data_pemesanan->row()->jml_pemesanan?></h2>
+                            <!-- <span>- 45.87</span> -->
                         </div>
                     </div>
-                    <canvas id="coin_sales3" height="100"></canvas>
+                    <!-- <canvas id="coin_sales3" height="100"></canvas> -->
                 </div>
             </div>
-        </div> -->
+        </div>
     </div>
     <div class="row">
         <div class="col-md-12">
